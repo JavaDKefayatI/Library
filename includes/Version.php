d03f918be77629ad38a91e4471f2824b55e38ae7
javadKefayati <javad936807@gmail.com>
2021-10-18 09:59:25 +0330

    add lastVersionFromServer

--- includes/Version.php
+++ includes/Version.php
@@ -3,7 +3,14 @@
 
 class Version
 {
-    public function lastVersion(Config_inc $db, string $table_name, string $column_name): int
+    /**
+     * @param Config_inc $db database
+     * @param string $table_name
+     * @param string $column_name
+     * @return int
+     */
+
+    public static function lastVersionFromDatabase(Config_inc $db, string $table_name, string $column_name): int
     {
         try {
             $query = "SELECT MAX(" . $column_name . ") FROM " . $table_name;
@@ -18,6 +25,23 @@ class Version
 
     }
 
+    /**
+     * @uses check_big_version
+     * @param $decodeInformation
+     * @return mixed
+     */
+    public static function lastVersionFromServer($decodeInformation)
+    {
+        //give first element for check with other
+        $temp = $decodeInformation[0];
+        for ($i = 1; $i < count($decodeInformation);
+             $i++) {
+            if ($temp < $decodeInformation[$i])
+                $temp = $decodeInformation[$i];
+        }
+        return $temp;
+
+    }
 
 
 }
