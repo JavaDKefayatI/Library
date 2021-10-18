a8932261f8605f14474e0fc2f7fa901b6ef6215f
javadKefayati <javad936807@gmail.com>
2021-10-18 16:11:55 +0330

    add new func and change state class

--- includes/Version.php
+++ includes/Version.php
@@ -3,21 +3,24 @@
 
 class Version
 {
+    private string $lastVersionInDatabase;
+    private string $lastNewNameVersion;
+
+
     /**
      * @param Config_inc $db database
      * @param string $table_name
      * @param string $column_name
-     * @return int
      */
 
-    public static function lastVersionFromDatabase(Config_inc $db, string $table_name, string $column_name): int
+    public function setLastVersionFromDatabase(Config_inc $db, string $table_name, string $column_name)
     {
         try {
             $query = "SELECT MAX(" . $column_name . ") FROM " . $table_name;
 
             $statement = $db->connect->query($query);
             // get and send all publishers
-            return $statement->fetchAll(PDO::FETCH_ASSOC)[0]["MAX(" . $column_name . ")"];
+            $this->lastVersionInDatabase = $statement->fetchAll(PDO::FETCH_ASSOC)[0]["MAX(" . $column_name . ")"];
 
         } catch (PDOException $e) {
             throw $e;
@@ -27,32 +30,53 @@ class Version
 
     /**
      * @param $decodeInformation
+     * @param bool $want_index
      * @return mixed
      * @uses check_big_version
      */
-    public static function lastVersionFromServer($decodeInformation)
+    public function lastVersionFromServer($decodeInformation, bool $want_index=false)
     {
-        //give first element for check with other
-        $temp = $decodeInformation[0]->nameVersion;
+        //give max version or index it  for check with other
+        $max = $decodeInformation[0]->nameVersion;
+        $index_max = 0;
         for ($i = 1; $i < count($decodeInformation);
              $i++) {
-            if ($temp < $decodeInformation[$i]->nameVersion)
-                $temp = $decodeInformation[$i]->nameVersion;
+            if ($max < $decodeInformation[$i]->nameVersion) {
+                $max = $decodeInformation[$i]->nameVersion;
+                $index_max = $i;
+            }
         }
-        return $temp;
+        if ($want_index == true)
+            return $index_max;
+        return $max;
 
     }
 
     /**
-     * @param Config_inc $connect database
      * @param string $url =>for example:'https://www.server.com/api.php'
      * @param int $current_version current version from database
      * @return bool has new version or hasn't
      */
 
-    public static function checkVersion(Config_inc $connect, string $url, int $current_version): bool
+    public function checkVersion(int $current_version, array $array_versions): bool
     {
-        // get information from server
+
+        //max version from server
+        $maxVersion = $this->lastVersionFromServer($array_versions);
+
+        //check current version with the version from server
+        if ($current_version < $maxVersion)
+            return true;
+        else
+            return false;
+    }
+
+    /**
+     * @param string $url
+     * @return array
+     */
+    public function versions(string $url): array
+    {
         // Initialize a CURL session.
         $ch = curl_init();
         // Return Page contents.
@@ -61,16 +85,39 @@ class Version
         curl_setopt($ch, CURLOPT_URL, $url);
         $server_information = curl_exec($ch);
 
-        //create array from json
-        $array_versions = json_decode($server_information);
-        //max version from server
-        $maxVersion = Version::lastVersionFromServer($array_versions);
-
-        //check current version with the version from server
-        if ($current_version < $maxVersion)
-            return true;
-        else
-            return false;
+        //create array from json and return
+        return json_decode($server_information);
 
     }
+
+    public function setInformationInDatabase(Config_inc $connect, array $array_of_versions)
+    {
+        //give last version index from database
+        $max_index = $this->lastVersionFromServer($array_of_versions, true);
+        //give information of version
+        $version = $array_of_versions[$max_index]->nameVersion;
+        $name = $array_of_versions[$max_index]->nameFile;
+        $describe = $array_of_versions[$max_index]->describe;
+        //set in database
+        $connect->insert("version", ['numberVersion', '`describe`', 'name'], [$version, $describe, $name]);
+        $this->lastNewNameVersion = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLastVersionInDatabase(): string
+    {
+        return $this->lastVersionInDatabase;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLastNewNameVersion(): string
+    {
+        return $this->lastNewNameVersion;
+    }
+
+
 }
