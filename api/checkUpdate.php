144348e5a61a1893ef58332200709e8a2b329808
javadKefayati <javad936807@gmail.com>
2021-10-18 16:12:30 +0330

    coding until download file

--- api/checkUpdate.php
+++ api/checkUpdate.php
@@ -1,21 +1,30 @@
+<pre>
 <?php
-
-// From URL to get webpage contents.
 include "../includes/Config_inc.php";
 include "../includes/Version.php";
 
-$connect = new Config_inc('library');
+$connect = new Config_inc("library");
 $version = new Version();
-
-//get current max version
-$current_v = $version->lastVersion($connect, "version", "numberVersion");
+$version->setLastVersionFromDatabase($connect, "version", "numberVersion");
 //create url for connecting server
 $url = Config_inc::getServer() . "/server/api.php";
+//get current max version
+$current_v = $version->getLastVersionInDatabase();
+
 // get information from server
-$result = $version::getSite($url);
+$array_versions = $version->versions($url);
+// check update
+$is_update = $version->checkVersion($current_v, $array_versions);
 
-echo $result;
+if ($is_update) {
+    //set new information for version in database
+    $version->setInformationInDatabase($connect, $array_versions);
+    echo "JAVAD";
+    //download file from server
+    exec("wget ".Config_inc::getServer()."/server/uploads/".$version->getLastNewNameVersion());
+
+}
 
 
-
-
+?>
+</pre>
