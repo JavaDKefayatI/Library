6a8006a6f054518abc81e067436199ebe14c90af
javadKefayati <javad936807@gmail.com>
2021-10-18 10:21:54 +0330

    add checkVersion function

--- includes/Version.php
+++ includes/Version.php
@@ -26,22 +26,51 @@ class Version
     }
 
     /**
-     * @uses check_big_version
      * @param $decodeInformation
      * @return mixed
+     * @uses check_big_version
      */
     public static function lastVersionFromServer($decodeInformation)
     {
         //give first element for check with other
-        $temp = $decodeInformation[0];
+        $temp = $decodeInformation[0]->nameVersion;
         for ($i = 1; $i < count($decodeInformation);
              $i++) {
-            if ($temp < $decodeInformation[$i])
-                $temp = $decodeInformation[$i];
+            if ($temp < $decodeInformation[$i]->nameVersion)
+                $temp = $decodeInformation[$i]->nameVersion;
         }
         return $temp;
 
     }
 
+    /**
+     * @param Config_inc $connect database
+     * @param string $url =>for example:'https://www.server.com/api.php'
+     * @param int $current_version current version from database
+     * @return bool has new version or hasn't
+     */
 
+    public static function checkVersion(Config_inc $connect, string $url, int $current_version): bool
+    {
+        // get information from server
+        // Initialize a CURL session.
+        $ch = curl_init();
+        // Return Page contents.
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        //grab URL and pass it to the variable.
+        curl_setopt($ch, CURLOPT_URL, $url);
+        $server_information = curl_exec($ch);
+
+        //create array from json
+        $array_versions = json_decode($server_information);
+        //max version from server
+        $maxVersion = Version::lastVersionFromServer($array_versions);
+
+        //check current version with the version from server
+        if ($current_version < $maxVersion)
+            return true;
+        else
+            return false;
+
+    }
 }
