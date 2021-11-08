63bdf5e30a3876fd6b04455f5dd4d52b73fa5390
javad <javad936807@gmail.com>
2021-11-08 17:23:31 +0330

    set new file for request user

---
+++ AllBook.php
@@ -0,0 +1,71 @@
+<?php
+include "Header.php";
+
+if (isset($_POST['request'])){
+    $id_book = $_POST['request'];
+
+
+}
+
+
+
+
+?>
+<script src="front/js/user/AllBook.js" ></script>
+
+<script>
+    // setTitle("title", "Library")
+    AllBook()
+</script>
+
+<main id="bodyTable " class="mb-4" style="">
+
+    <div class=" mb-4  container mt-4" id="body">
+
+
+        <table id="example" class=" w-75 cell-border mb-4 ">
+
+        <thead class="  text-center">
+
+        <tr class="text-center">
+            <th class="">Id</th>
+            <th>Name</th>
+            <th>Author</th>
+            <th>Print year</th>
+            <th>Status</th>
+            <th class=" "></th>
+        </tr>
+
+        </thead>
+
+        <tbody class="text-center" id="tb">
+
+        </tbody>
+
+        <tfoot>
+        <tr class="text-center   ">
+            <th>Id</th>
+            <th>Name</th>
+            <th>Author</th>
+            <th>Print year</th>
+            <th>Status</th>
+            <th></th>
+        </tr>
+        </tfoot>
+
+        </table>
+    </div>
+    <div style="margin-right:21%" class=" container d-flex justify-content-end ">
+        <a href="CreateOrEditBook.php ">
+            <button class='btn  btn-secondary w-100 p-3  '>
+                Create a book
+            </button>
+        </a>
+    </div>
+
+</main>
+
+
+<?php
+include "Footer.php";
+?>
