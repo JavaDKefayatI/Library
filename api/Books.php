3140782ca71643eb9cecd117ed060d87ae8e0b49
javad <javad936807@gmail.com>
2021-11-08 17:24:42 +0330

    new api for read all books

---
+++ api/Books.php
@@ -0,0 +1,18 @@
+<?php
+include "../includes/Config_inc.php";
+include "../includes/Users.php";
+
+$connect = new Config_inc("library2");
+$user = new Users();
+
+try {
+    if ($user->isLogIn($connect))
+        header('Location:sign/SignIn.php');
+
+} catch (Exception $e) {
+
+}
+
+$books = $connect->selectOrSearch("books", ['*']);
+echo(json_encode($books));
+
