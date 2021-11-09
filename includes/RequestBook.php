ebd14d031a1835997c5f5570333f7411e549c010
javad <javad936807@gmail.com>
2021-11-09 17:12:26 +0330

    add some func

--- includes/RequestBook.php
+++ includes/RequestBook.php
@@ -9,4 +9,35 @@ class RequestBook
             "id_book='" . $id_book . "'");
 
     }
+
+    /**
+     * @param Config_inc $db
+     * @param string $id_user
+     * @param string $id_book
+     */
+    public function setRequest(Config_inc $db, string $id_user, string $id_book): void
+    {
+        //this statement changed status (available or wait to wait )
+        $db->edit("books",['status'=>'1'],"Id = ".$id_book);
+        //set data to request table
+        $db->insert("requestbook", ["id_user", "id_book"], [$id_user, $id_book]);
+    }
+
+    /**
+     * @param Config_inc $db database
+     * @param string $id_user
+     * @param string $id_book
+     * @return bool return true if not exist this id(book and user)
+     */
+
+    public function isRequest(Config_inc $db, string $id_user, string $id_book): bool
+    {
+
+        $req = $db->selectOrSearch("requestbook", ["id"], "id_user=" . $id_user . " and id_book =" . $id_book);
+
+        if (count($req) == 0) return false;
+        return true;
+    }
+
+
 }
