c225ad779fbf0720d429e590ed15d676776d2105
javad <javad936807@gmail.com>
2021-11-17 09:31:27 +0330

    create api with inner join

--- api/AllRequest.php
+++ api/AllRequest.php
@@ -1,9 +1,11 @@
 <?php
 include "../includes/Config_inc.php";
 include "../includes/Users.php";
-
+include "../includes/Admin.php";
 $db = new Config_inc("library2");
 $user = new Users();
+$admin = new Admin();
+
 
 try {
     if ($user->isLogIn($db))
@@ -12,17 +14,7 @@ try {
 } catch (Exception $e) {
 
 }
-$req = $db->selectOrSearch("requestbook", ['*']);
-//echo $req[0]["id"];
-//var_dump($req);
-for ($i = 0; $i < count($req); $i++) {
-    $id_user = $req[$i]["id_user"];
-    $id_book = $req[$i]["id_book"];
-    $user = $db->selectOrSearch("users", ["username"], "id = " . $id_user);
-    $name_book = $db->selectOrSearch("books", ["Name","Author"], "Id = " . $id_book);
-
-    $req[$i] = $req[$i] + $user[0]+$name_book[0];
-}
+$req = $admin->createApi($db);
 
 echo(json_encode($req));
 
