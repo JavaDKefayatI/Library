bd8e30360ff558c2ba2310aaf0fe44d85bd134d2
javad <javad936807@gmail.com>
2021-11-17 09:29:35 +0330

    changed

--- includes/Admin.php
+++ includes/Admin.php
@@ -10,10 +10,14 @@ class Admin
     public function setAccept(Config_inc $db, string $id_request)
     {
 
-        $db->edit("requestbook", ["is_accept" => "1"], "id=" . $id_request);
+        $db->edit("requestbook", ["status" => "1"], "id=" . $id_request);
+        $db->setCurrentTime("requestbook", "time_check", "id=" . $id_request);
+
         $id_book = $db->selectOrSearch("requestbook", ["id_book"], "id=" . $id_request)[0]["id_book"];
         //reject another request
-        $db->edit("requestbook", ["is_accept" => "-1"], "id_book=" . $id_book . " and is_accept=0");
+        $db->edit("requestbook", ["status" => "-1"], "id_book=" . $id_book . " and status=0");
+        $db->setCurrentTime("requestbook", "time_check", "id_book=" . $id_book . " and status=0");
+
         //change status book
         $db->edit("books", ["status" => "2"], "Id =" . $id_book);
 
@@ -24,10 +28,10 @@ class Admin
      * @param string $id_req
      * @return bool
      */
-    public function checkAccept(Config_inc $db, string $id_req): bool
+    public function checkRequest(Config_inc $db, string $id_req): bool
     {
-        $is_accept = $db->selectOrSearch("requestbook", ["is_accept"], "id=" . $id_req)[0]["is_accept"];
-        if ($is_accept == "1" || $is_accept == "-1") return false;
+        $status = $db->selectOrSearch("requestbook", ["status"], "id=" . $id_req)[0]["status"];
+        if ($status == "1" || $status == "-1" || $status == "2") return false;
         return true;
     }
 
@@ -37,13 +41,43 @@ class Admin
      */
     public function reject(Config_inc $db, string $id_request)
     {
-        $db->edit("requestbook", ["is_accept" => "-1"], "id=" . $id_request);
+        $db->edit("requestbook", ["status" => "-1"], "id=" . $id_request);
+        $db->setCurrentTime("requestbook", "time_check", "id=" . $id_request);
+
         $id_book = $db->selectOrSearch("requestbook", ["id_book"], "id=" . $id_request)[0]["id_book"];
-        $anotherExist = $db->isExist("requestbook", ["id"], "id_book=" . $id_book . " and is_accept=0");
+        $anotherExist = $db->isExist("requestbook", ["id"], "id_book=" . $id_book . " and status=0");
 
         if (!$anotherExist)
             $db->edit("books", ["status" => "0"], "Id =" . $id_book);
 
+    }
+
+    public function createApi($db)
+    {
+        try {
+            $query = "SELECT
+                requestbook.id,
+                books.Name,
+                requestbook.id_user ,
+                books.Author,
+                requestbook.id_book ,
+                requestbook.status,
+                requestbook.time_check,
+                requestbook.time_request,
+                requestbook.time_return,
+                users.username
+                FROM((requestbook
+                INNER JOIN users ON users.id = requestbook.id_user)
+                INNER JOIN books ON books.Id = requestbook.id_book);";
+
+            $statement = $db->connect->query($query);
+
+            // get and send all publishers
+            return $statement->fetchAll(PDO::FETCH_ASSOC);
+
+        } catch (PDOException $e) {
+            throw $e;
+        }
     }
 
 
