41abab11f82e422c4007e7b5ea0222a1f24f76a8
javad <javad936807@gmail.com>
2021-11-29 11:46:18 +0330

    add dive tag

--- admin/History.php
+++ admin/History.php
@@ -1,16 +1,17 @@
 <?php
 
 include "Header.php";
-include "includes/RequestBook.php";
+include "../includes/RequestBook.php";
 
 
 
 
 ?>
-<script src="front/js/History.js"></script>
+<script src="../front/js/admin/History.js"></script>
 
 <script>
     // setTitle("title", "Library")
+    admin("tb")
 
 </script>
 
@@ -18,11 +19,7 @@ include "includes/RequestBook.php";
 
 
     <div class="container">
-        <div class="container mt-6 mb-4 " style="display: inline">
-            <h2 style="display: inline"> Choose history :</h2>
-            <button class="mr-5 btn btn-info w-25 ml-5" onclick="admin('tb')">Admin</button>
-            <button class="mr-5 btn btn-info w-25" onclick="user(<?= $user->getId() ?> , 'tb')">User</button>
-        </div>
+
         <table class="table table-bordered mt-4">
             <thead class="table-primary">
             <tr>
@@ -34,8 +31,13 @@ include "includes/RequestBook.php";
                 <th>Time checked</th>
             </tr>
             </thead>
-            <tbody id="tb">
-
+            <tbody class="" id="tb">
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
             </tbody>
             <tfoot class="table-primary">
             <tr>
