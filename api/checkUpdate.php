935001807c05e95b865ff2325765005e50c52cae
javad <javad936807@gmail.com>
2021-11-29 11:47:12 +0330

    change connect name with db name

--- api/checkUpdate.php
+++ api/checkUpdate.php
@@ -1,12 +1,13 @@
-c<?php
+<?php
 include "../includes/Config_inc.php";
 include "../includes/Version.php";
 
-$connect = new Config_inc("library2");
+$db= new Config_inc("library2");
+
+
 
 $version = new Version();
-
-$version->setLastVersionFromDatabase($connect, "version", "numberVersion");
+$version->setLastVersionFromDatabase($db, "version", "numberVersion");
 
 //get current max version
 $maxVersion = $version->getLastVersionInDatabase();
@@ -16,14 +17,12 @@ $url = Config_inc::getServer() . "/server/api/CheckUpdate.php?version=" . $maxVe
 // get information from server
 $information = $version->informationFromServer($url);
 // check update
-
 if ($information["status"] == 1) {
 
     //set new information for version in database
-    $version->setInformationInDatabase($connect, $information);
+    $version->setInformationInDatabase($db, $information);
     $version->DownloadFileAndSet();
     echo json_encode(["status" => 1]);
 } else
     echo json_encode(["status" => 0]);
 
-?>
